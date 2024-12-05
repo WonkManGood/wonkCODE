d42c0ba40bdf2e270b66ebb49fc03896cb7411a0
unknown <ezrajw@pm.me>
2024-12-05 00:37:50 -0500

    12.05

--- cs50/Z - Final/project.py
+++ cs50/Z - Final/project.py
@@ -1,15 +1,22 @@
-import csv as c
-from io import StringIO
+# /// Globally initializes my attempt at a package.
+try:
+    import wonkCSV
+    c = wonkCSV.wonkCSV
+except: raise ModuleNotFoundError("Missing wonkCSV. Please validate build with source.")
+
+# /// Initializes global placeholder for cipher lines.
+wonkCIPHER = []
 
 def main():
 
 # /// Validates you have cipher.
-    try: cipher = c.reader(open('dictionary.csv', 'r'))
-    except: raise(LookupError('Couldn\'t find cipher. Validate build and run program in build folder.'))
+    try: cipher = c.wonkCSV('dictionary.csv')
+    except: raise(LookupError('Couldn\'t find cipher. Validate build with source and/or run program in build folder.'))
 
+# /// Prompts user for usage of program and returns function's results.
     print('''
     wonkCIPHER: -- 0.1
-    a simple visual cipher
+    a simple visual cipher - inspired by randomart in recent ssh builds
 
 
     Encrypt String:    0
@@ -17,6 +24,13 @@ def main():
     Generate Random:   2
     ''')
     choice = input("Choice (0-2): ")
+    match choice:
+        case '0':
+            print(encrypt(input("Enter string: ")))
+        case '1':
+            print(decrypt())
+        case '2':
+            print(random())
 
 
 def encrypt():
@@ -24,12 +38,23 @@ def encrypt():
 
 
 def decrypt():
-    ...
+    clear()
+    i = 0
+    for i in range(5):
+        i = i + 1
+        line = input(f'Enter line #{i}: ')
+        wonkCIPHER.append(line)
 
 
 def random():
     import random as r
     ...
+
+# /// Used to keep terminal looking clean.
+def clear():
+    from os import system, name
+    if name == 'nt': system('cls')
+    else: system('clear')
 
 
 if __name__ == "__main__":
