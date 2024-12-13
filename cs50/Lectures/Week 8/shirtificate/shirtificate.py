60ecafe7f7d9ab92f6f0f39ddcbb7238a7d14138
WonkManGood <ezrajw@pm.me>
2024-12-12 23:58:55 -0500

    12.13

--- cs50/Lectures/Week 8/shirtificate/shirtificate.py
+++ cs50/Lectures/Week 8/shirtificate/shirtificate.py
@@ -1,9 +1,31 @@
 from fpdf import FPDF
 
-pdf = FPDF(orientation='P', format='A4')
-pdf.add_page()
-pdf.image('./shirtificate.png')
-# pdf.set_font(family='Courier')
-# pdf.text(105, 146.5, text='CS50 Shirtificate')
+class PDF(FPDF):
+    def header(self):
+        self.image("./shirtificate.png", 10, 70, 190)
+        self.set_font("helvetica", "", 48)
+        self.cell(0, 57, "CS50 Shirtificate", align="C")
+        self.ln(20)
 
-pdf.output('./shirtificate.pdf')
+def shirt(s):
+    pdf = PDF()
+    pdf.add_page(orientation="portrait", format="a4")
+    pdf.set_font("helvetica", size=24)
+    pdf.set_text_color(255, 255, 255)
+    pdf.cell(0, 213, f"{s} took CS50", align="C")
+    pdf.output("shirtificate.pdf")
+
+def main():
+    name = input("Name: ")
+    pdf = PDF()
+    pdf.add_page(orientation="portrait", format="a4")
+    pdf.set_font("helvetica", size=24)
+    pdf.set_text_color(255, 255, 255)
+    pdf.cell(0, 213, f"{name} took CS50", align="C")
+    pdf.output("shirtificate.pdf")
+
+if __name__ == "__main__":
+    main()
+
+
+#this is copied, redo
