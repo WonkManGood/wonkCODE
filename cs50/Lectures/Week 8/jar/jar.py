30b4d7933580a96bacb012ef58a9bd7caf4cd2b5
WonkManGood <ezrajw@pm.me>
2024-12-12 23:08:38 -0500

    '12.13'

--- cs50/Lectures/Week 8/jar/jar.py
+++ cs50/Lectures/Week 8/jar/jar.py
@@ -1,23 +1,29 @@
 class Jar:
-    def __init__(self, size):
-        self.capacity = 12
-        self.size = size
+    def __init__(self, size=0, capacity=12):
+        if int(size) < 0: raise ValueError('cannot have a less than zero size')
+        else: self.size = int(size)
+        self.capacity = capacity
 
     def __str__(self):
-        return str('🍪' * self.size)
+        if self._size > 0: return (f'🍪' * self.size)
+        if self._size == 0: return (f'')
 
     def deposit(self, n):
-        if n + self.size >=
+        if self._size + n > self._capacity: raise ValueError('Inserted too many cookies!')
+        else: self._size = int(self._size + n)
 
     def withdraw(self, n):
-        ...
+        if self._size - n < 0: raise ValueError('Can\'t go negative with your cookies sir!') 
+        else: self._size = int(self._size - n)
 
     @property
     def capacity(self):
         return self._capacity
-    
+
     @capacity.setter
     def capacity(self, capacity):
+        if self._size > capacity:
+            raise ValueError("Too many cookies!!!")
         self._capacity = capacity
 
     @property
@@ -26,8 +32,7 @@ class Jar:
 
     @size.setter
     def size(self, size):
-        if size > 12:
-            return ValueError("Too many cookies!!!")
+        if size >= 12:
+            raise ValueError('Too many cookies!!!')
+        if size < 0: raise ValueError('Can\'t have negative cookies.')
         self._size = size
-
-print(Jar(1))
--- cs50/Lectures/Week 8/jar/test_jar.py
+++ cs50/Lectures/Week 8/jar/test_jar.py
@@ -2,21 +2,29 @@ from jar import Jar
 
 
 def test_init():
-    ...
+    jar = Jar().__init__()
+    assert Jar().__init__() == jar
 
 
 def test_str():
-    jar = Jar()
-    assert str(jar) == ""
-    jar.deposit(1)
+    jar = Jar(1)
     assert str(jar) == "🍪"
-    jar.deposit(11)
+    jar.deposit(1)
+    assert str(jar) == "🍪🍪"
+    jar.deposit(10)
     assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
 
 def test_deposit():
-    ...
+    jar = Jar(1)
+    assert str(jar) == "🍪"
+    jar.deposit(1)
+    assert str(jar) == "🍪🍪"
+    jar.deposit(10)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
 
 def test_withdraw():
-    ...
+    jar = Jar(1)
+    jar.withdraw(1)
+    assert str(jar) == ''
